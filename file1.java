b6fe30e4041158ed37ffacc79a92ca0c1547a078
Subham Kumbhare <105261620+Shubham-150-k@users.noreply.github.com>
2023-04-06 12:19:41 +0530

    commit4

--- file1.java
+++ file1.java
@@ -1,1 +1,29 @@
-FFile1
+public class Person{
+    String name;
+    int age;
+
+    Person(String name,int age)
+    {
+        this.name=name;
+        this.age=age;
+    }
+}
+
+public class Student extends Person{
+    int roll_no;
+    float marks;
+
+    Student(String name,int age,int roll_no,float marks)
+    {
+        super(name,age);
+        this.roll_no=roll_no;
+        this.marks=marks;
+    }
+
+   public static void main(String args[])
+   {
+        Student s1=new Student("Shubham",25,12,89.22f);
+        System.Out.println(s1);
+   }
+    
+}
