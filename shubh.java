230cd5067ef0961f7ff6985fe9b5d78ee8458fee
Subham Kumbhare <105261620+Shubham-150-k@users.noreply.github.com>
2023-04-19 11:29:36 +0530

    d-commit

--- shubh.java
+++ shubh.java
@@ -1,6 +1,41 @@
-public Class shubh{
-    public static void main(String []args)
+public class abstract Account_Detail{
+ int account_number;
+ String Name;
+ int Mobile_number;
+
+ Account_Detail(int account_number,String Name,int Mobile_number){
+    this.account_number=account_number;
+    this.name=name;
+    this.Mobile_number=Mobile_number;
+ }
+}
+
+public class Simple_interest extends Account_Detail{
+
+    double interest;
+    int year;
+    double amount;
+
+    Simple_interest(double interest,int year,double amount,int account_number,String name,int Mobile_number)
     {
-        System.out.println("Hello World");
+        super(account_number,name,Mobile_number);
+        this.interest=interest;
+        this.year=year;
+        this.amount=amount;
+    }
+    //public static void main(String []args)   
+}
+
+public class Compound_interest{
+    int Compound_interest;
+    int year;
+    double amount;
+
+    Compound_interest(int Compound_interest,int year,double amount,int account_number,String name,int Mobile_number)
+    {
+        super(account_number,name,Mobile_number);
+        this.Compound_interest=Compound_interest;
+        this.year=year;
+        this.amount=amount;
     }
 }
